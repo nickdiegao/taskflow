b16b9708fa3f50f1182c459952453a1281cd65bf
nickdiegao <nickdiegao@gmail.com>
2026-02-22 16:38:53 -0300

    chore: configure TypeORM, BullModule and enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST!,
+        port: parseInt(process.env.DATABASE_PORT!),
+        username: process.env.DATABASE_USER!,
+        password: process.env.DATABASE_PASSWORD!,
+        database: process.env.DATABASE_NAME!,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+
+    BullModule.forRootAsync({
+      useFactory: () => ({
+        redis: {
+          host: process.env.REDIS_HOST!,
+          port: parseInt(process.env.REDIS_PORT!),
+        },
+      }),
+    }),
+  ],
 })
 export class AppModule {}
