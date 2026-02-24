cdc0b28b4a5416bebefefcd9f550bf8fe8b90a6e
nickdiegao <nickdiegao@gmail.com>
2026-02-24 11:44:53 -0300

    feat: implement TasksController with CRUD endpoint by JWT

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Patch,
+    Delete,
+    Body,
+    Param,
+    ParseIntPipe,
+    UseGuards,
+    Request,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { TaskStatus, TaskPriority } from './task.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('tasks')
-export class TasksController {}
+@UseGuards(JwtAuthGuard)
+export class TasksController {
+    constructor(private readonly tasksService: TasksService) {}
+
+    @Post()
+    async create(
+        @Body() dto: { title: string; description?: string; priority?: TaskPriority, dueDate: Date },
+        @Request() req: any,
+    ) {
+        return this.tasksService.create(dto, req.user.id);
+    }
+
+    @Get()
+    async findAll(@Request() req:any) {
+        return this.tasksService.findAll(req.user.id);
+    }
+
+    @Get(':id')
+    async findOne(@Param('id', ParseIntPipe) id: number, @Request() req: any) {
+        return this.tasksService.findOne(id, req.user.id);
+    }
+
+    @Put(':id')
+    async update(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() dto: { title?: string; description?: string; priority?: TaskPriority; dueDate: Date },
+        @Request() req: any
+    ) {
+        return this.tasksService.update(id, dto, req.user.id)
+    }
+
+    @Delete(':id')
+    async remove(@Param('id', ParseIntPipe) id: number, @Request() req: any) {
+        return this.tasksService.remove(id, req.user.id)
+    }
+
+    @Patch(':id/status')
+    async updateStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status') status: TaskStatus,
+        @Request() req: any
+    ) {
+        return this.tasksService.updateStatus(id, status, req.user.id);
+    }
+}
